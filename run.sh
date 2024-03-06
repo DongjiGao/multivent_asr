d7269b09271b194c0f0e3eb00472b4aa8abb54ba
Dongji Gao <dgao5@jhu.edu>
2024-03-06 15:20:47 -0500

    small fix

--- run.sh
+++ run.sh
@@ -29,17 +29,17 @@ languages=(
     en
 )
 
-#for event in ${events[@]}; do
-#    for language in ${languages[@]}; do
-#        log "Aligning ${event} ${language}"
-#        ./conformer_ctc/otc_alignment.py \
-#            --event "${event}" \
-#            --language "${language}" \
-#            --method "${decoding_method}" \
-#            --exp-dir "${exp_dir}" \
-#            --lang-dir "${lang_dir}" 
-#   done
-#done
+for event in ${events[@]}; do
+    for language in ${languages[@]}; do
+        log "Aligning ${event} ${language}"
+        ./conformer_ctc/otc_alignment.py \
+            --event "${event}" \
+            --language "${language}" \
+            --method "${decoding_method}" \
+            --exp-dir "${exp_dir}" \
+            --lang-dir "${lang_dir}" 
+   done
+done
 
 for event in ${events[@]}; do
     for language in ${languages[@]}; do
