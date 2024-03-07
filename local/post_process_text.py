ece6e322f4ecdedfa607fa0b04a7e010c9f0b13e
Dongji Gao <dgao5@jhu.edu>
2024-03-06 19:20:39 -0500

    add scripts to convert text back to whisper style

--- local/post_process_text.py
+++ local/post_process_text.py
@@ -5,6 +5,8 @@
 import argparse
 from pathlib import Path
 
+from kaldialign import align
+
 
 def arg_parser():
     parser = argparse.ArgumentParser()
@@ -14,6 +16,11 @@ def arg_parser():
         help="path to the text file",
     )
     parser.add_argument(
+        "--whisper-text",
+        type=str,
+        help="path to the whisper raw text file",
+    )
+    parser.add_argument(
         "--output-dir",
         type=str,
         help="path to the output directory",
@@ -21,32 +28,57 @@ def arg_parser():
     return parser.parse_args()
 
 
+def align_text(text_list, whisper_text_list, error_token="ERR"):
+
+    for i, word in enumerate(text_list):
+        if word == "<star>":
+            text_list[i] = "*"
+
+    result = []
+    ali = align(text_list, whisper_text_list, error_token)
+    for word, whisper_word in ali:
+        if word == "*":
+            aligned_word = word
+        elif word == error_token:
+            assert whisper_word != error_token
+            aligned_word = whisper_word
+        elif whisper_word == error_token:
+            assert word != error_token
+            aligned_word = word
+        else:
+            aligned_word = whisper_word
+
+        result.append(aligned_word)
+    return result
+
 def main():
     args = arg_parser()
     text_file = Path(args.text)
+    whisper_text_file = Path(args.whisper_text)
     output_dir = Path(args.output_dir)
 
-    result = []
+    # read ans store whisper text
+    whisper_text = {}
+    with open(whisper_text_file, "r") as wt:
+        for line in wt:
+            utt_id, text = line.strip().split(maxsplit=1)
+            assert utt_id not in whisper_text
+            whisper_text[utt_id] = text
+
     with open(text_file, "r") as tf:
         with open(output_dir / "otc_aligned_text.txt", "w") as oat:
             for line in tf:
-                line = line.strip().split()
-                utt_id = line[0]
-                cur_text = [utt_id]
+                utt_id, text = line.strip().split(maxsplit=1)
+                assert utt_id in whisper_text
 
-                for word in line[1:]:
-                    if word == "<star>":
-                        cur_text.append("*")
-                    elif word == "I":
-                        cur_text.append(word)
-                    else:
-                        cur_text.append(word.lower())
+                text_list = text.lower().split()
+                whisper_text_list = whisper_text[utt_id].split()
 
-                result.append(" ".join(cur_text))
+                aligned_text_list = align_text(text_list, whisper_text_list)
+                aligned_text_list[0] = aligned_text_list[0].capitalize()
 
-            result = sorted(result)
-            for text in result:
-                oat.write(f"{text}\n")
+                oat.write(f"{utt_id} {' '.join(aligned_text_list)}\n")
+
 
 
 if __name__ == "__main__":
